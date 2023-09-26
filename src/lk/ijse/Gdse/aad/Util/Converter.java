acfd4486e4cd2ea64651d4bd8a392a94b6c44591
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-09-26 20:59:38 +0530

    update converter

--- src/lk/ijse/Gdse/aad/Util/Converter.java
+++ src/lk/ijse/Gdse/aad/Util/Converter.java
@@ -2,8 +2,10 @@ package lk.ijse.Gdse.aad.Util;
 
 import lk.ijse.Gdse.aad.Dto.CustomerDto;
 import lk.ijse.Gdse.aad.Dto.ItemDto;
+import lk.ijse.Gdse.aad.Dto.OrderDto;
 import lk.ijse.Gdse.aad.Entity.Customer;
 import lk.ijse.Gdse.aad.Entity.Item;
+import lk.ijse.Gdse.aad.Entity.Order;
 
 public class Converter {
     public CustomerDto fromCustomer(Customer customer){
@@ -22,13 +24,13 @@ public class Converter {
         return new Item(itemDto.getItemid(),itemDto.getDescription(), itemDto.getUnitprice(), itemDto.getQty());
     }
 
-//    public OrderDTO fromOrder(Order order){
-//        return new OrderDTO(order.getOid(),order.getDate(),order.getCid(), order.getStatus(), order.getPrice());
-//    }
-//
-//    public Order toOrder(OrderDTO orderDTO){
-//        return new Order(orderDTO.getOid(),orderDTO.getDate(),orderDTO.getCid(), orderDTO.getStatus(),orderDTO.getPrice());
-//    }
+    public OrderDto fromOrder(Order order){
+        return new OrderDto(order.getO_id(),order.getC_id(),order.getDate(),order.getTotal(),order.getDiscount(),order.getSubtotal());
+    }
+
+    public Order toOrder(OrderDto orderDTO){
+        return new Order(orderDTO.getO_id(),orderDTO.getC_id(),orderDTO.getDate(),orderDTO.getTotal(),orderDTO.getDiscount(),orderDTO.getSubtotal());
+    }
 //
 //    public PlaceOrderDTO fromPlaceOrder(PlaceOrder placeOrder){
 //        return new PlaceOrderDTO(placeOrder.getOid(),placeOrder.getDate(),placeOrder.getCid(),placeOrder.getStatus(),placeOrder.getPrice(),placeOrder.getOrderDetails());
